93314b113de5cfcd2129a38eb70348bd89504492
Kyle Lee <Kylecrlee95@gmail.com>
2019-01-29 10:33:02 -0600

    moved onto two sum

--- wrap.spec.js
+++ wrap.spec.js
@@ -1,8 +1,11 @@
-const { expect } = require('chai');
-const wrap = require('./wrap');
+const { expect } = require("chai");
+const wrap = require("./wrap");
 
-describe('wrap', () => {
-  it('Returns empty string if empty string was provided', () => {
-    expect(wrap('', 10)).to.equal('');
+describe("wrap", () => {
+  it("Returns empty string if empty string was provided", () => {
+    expect(wrap("", 10)).to.equal("");
+  });
+  it('It returns "invalid length" if charNum is < 0 or charNum > the length of the string', () => {
+    expect(wrap("ab", 1)).to.equal(`${"\n"}a${"\n"}b`);
   });
 });
